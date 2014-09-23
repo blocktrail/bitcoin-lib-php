5e7fa373a66439aa60b945e3ee663c3cadc39875
Ruben de Vries <ruben@rubensayshi.com>
2014-09-23 13:42:29 +0200

    added signing of transaction to bip32 example

--- examples/bip32.php
+++ examples/bip32.php
@@ -1,6 +1,7 @@
 <?php
 
 use BitWasp\BitcoinLib\BIP32;
+use BitWasp\BitcoinLib\RawTransaction;
 
 require_once(__DIR__. '/../vendor/autoload.php');
 
@@ -26,3 +27,41 @@ echo "             : ".BIP32::key_to_address($pub[0])."\n";
 $nextpub = BIP32::build_key($pub, '0');
 echo  "Child key\n";
 echo " {$nextpub[1]}      : {$nextpub[0]}\n";
+
+/////////////////////////////
+// Parameters for creation..
+// Set up inputs here
+$inputs = array(
+    array(
+        'txid' => '6737e1355be0566c583eecd48bf8a5e1fcdf2d9f51cc7be82d4393ac9555611c',
+        'vout' => 0
+    )
+);
+// Set up outputs here.
+$outputs = array( '12jYGamrswiVAenpDKRJwytRya7PuFKbWv' => "0.00015");
+
+////////////////////////////
+// Parameters for signing.
+// Create JSON inputs parameter
+// - These can come from bitcoind, or just knowledge of the txid/vout/scriptPubKey,
+//   and redeemScript if needed.
+$json_inputs = json_encode(
+    array(
+        array(
+            'txid' => '6737e1355be0566c583eecd48bf8a5e1fcdf2d9f51cc7be82d4393ac9555611c',
+            'vout' => 0,
+            // OP_DUP OP_HASH160 push14bytes       PkHash      OP_EQUALVERIFY OP_CHECKSIG
+            'scriptPubKey' => '76a914'.'1303b2ac55afee338764702d762849cc91e5bf76'.'88ac')
+    )
+);
+// Private Key
+$wallet = array();
+BIP32::bip32_keys_to_wallet($wallet, array($key), '00');
+
+// Create raw transaction
+$raw_transaction = RawTransaction::create($inputs, $outputs);
+
+// Sign the transaction
+$sign = RawTransaction::sign($wallet, $raw_transaction, $json_inputs);
+print_r($sign);echo "\n";
+
